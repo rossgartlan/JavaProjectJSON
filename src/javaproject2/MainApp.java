9613852c2a1916277ff76e21519173727b6ca205
[rossgartlan] <[D00187999@student.dkit.ie]>
2017-08-25 12:55:27 +0100

    test commit

--- src/javaproject2/MainApp.java
+++ src/javaproject2/MainApp.java
@@ -11,7 +11,7 @@ import javaproject2.Person;
 
 public class MainApp
 {
-
+    // test to see if commit works
     public static void main(String[] args)throws IOException
     {
         Map<String, Person> hashMap = new HashMap<>();
